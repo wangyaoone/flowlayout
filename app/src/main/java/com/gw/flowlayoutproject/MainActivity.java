11bfc02d0d1c806d4fc26ca7a2aaa379d42e1002
wy <1254769314@qq.com>
2020-08-18 14:48:54 +0800

    Initial commit

---
+++ app/src/main/java/com/gw/flowlayoutproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gw.flowlayoutproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gw.flowlayoutlibrary.FlowLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
